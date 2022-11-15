cb44d14f34ea1c09c624d7345dd1393ee25efef4
Oleksandra <predictormagnolia@gmail.com>
2022-11-16 00:11:19 +0200

    Add project files.

---
+++ WindowsProject6/WindowsProject6.cpp
@@ -0,0 +1,154 @@
+﻿// WindowsProject6.cpp : Defines the entry point for the application.
+//
+
+#include "framework.h"
+#include "WindowsProject6.h"
+#include <time.h>
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+HWND hList, hEdit;
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc);
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    //wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWSPROJECT6));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WINDOWSPROJECT6);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Store instance handle in our global variable
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+
+BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wp, LPARAM lp)
+{
+    TCHAR str[100];
+    int RightAnswers = 0;
+    int MaxRightAnswers = 14;
+    int ArrSize = 10 + rand() % 20;
+    int* Arr=new int[ArrSize];
+    int TryNumber,Sum=0,Multiply=1,Average;
+    static int Order = 1;
+    for (int i = 0; i < ArrSize; i++) {
+        TryNumber = rand() % 21 - 10;
+        if (TryNumber == 0) {
+            i--;
+            continue;
+        }
+        else {
+            Arr[i] = TryNumber;
+            Sum += Arr[i];
+            Multiply *= Arr[i];
+        }
+    }
+    Average = Sum / ArrSize;
+
+    switch (message)
+    {
+    case WM_CLOSE:
+        EndDialog(hWnd, 0);
+        return TRUE;
+
+    case  WM_INITDIALOG: {
+        hList = GetDlgItem(hWnd, IDC_LIST2);
+        hEdit = GetDlgItem(hWnd, IDC_EDIT1);
+        return TRUE;
+    }
+
+    case WM_COMMAND: {
+        if (Order == 0) {
+            LRESULT Result = SendDlgItemMessage(hWnd, IDC_RADIO1, BM_GETCHECK, 0, 0);
+            if (Result == BST_CHECKED) {
+                _stprintf_s(str, TEXT("%d"), Sum);
+                SetWindowText(hEdit, str);
+            }
+            Result = SendDlgItemMessage(hWnd, IDC_RADIO2, BM_GETCHECK, 0, 0);
+            if (Result == BST_CHECKED) {
+
+            }
+            Result = SendDlgItemMessage(hWnd, IDC_RADIO3, BM_GETCHECK, 0, 0);
+            if (Result == BST_CHECKED) {
+
+            }
+        }
+
+        if (LOWORD(wp) == IDC_START) {
+            if (Order == 1) {
+                for (int i = 0; i < ArrSize; i++) {
+                    _stprintf_s(str, TEXT("%d"), Arr[i]);
+                    SendMessage(hList, LB_ADDSTRING, 0, LPARAM(str));
+                }
+                Order = 0;
+            }
+            else {
+                SendMessage(hList, LB_RESETCONTENT, 0, 0);
+                Order = 1;
+            }
+
+        }
+        
+
+    }
+    return TRUE;
+    }
+    return FALSE;
+}
---
+++ WindowsProject6/resource.h
@@ -0,0 +1,35 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by WindowsProject6.rc
+//
+#define IDC_MYICON                      2
+#define IDD_WINDOWSPROJECT6_DIALOG      102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_WINDOWSPROJECT6             107
+#define IDI_SMALL                       108
+#define IDC_WINDOWSPROJECT6             109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define IDC_BUTTON1                     1000
+#define IDC_START                       1000
+#define IDC_RADIO1                      1003
+#define IDC_RADIO2                      1004
+#define IDC_RADIO3                      1005
+#define IDC_LIST2                       1007
+#define IDC_EDIT1                       1008
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
